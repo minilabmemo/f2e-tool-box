bd68418c2c4dd917d83ebdf86ffc2f8973f75654
minilabmemo <minilabmemo@gmail.com>
2023-12-01 12:51:02 +0800

    Fix: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,32 @@ export default function Home() {
   return (
     <main className='container m-auto bg-blue-300 flex flex-col h-screen p-4'>
       <h1 className='text-2xl	font-bold'>F2E Tool Box</h1>
+      <div className="border-b-4 border-slate-700 p-4 pb-6 mb-4">Play - Pixel Text Using :) </div>
       <div className='flex'>
 
-        <ul>
-          <li>  <Link href={`/tool/font`}>font</Link></li>
-          <li>  <Link href={`/tool/graph`}>graph</Link></li>
-          <li> <Link href={`/tool/screen`}>screen</Link></li>
-          <li> <Link href={`/showcase/map`}>map</Link></li>
+        <ul className="list-disc ml-4">
+          <li className="mb-2">tool
+            <ul className="list-disc ml-4">
+              <li><Link href={`/tool/font`}>font</Link></li>
+              <li><Link href={`/tool/screen`}>screen</Link></li>
+            </ul>
+          </li>
+          <li className="mb-2">showcase
+            <ul className="list-disc ml-4">
+              <li><Link href={`/showcase/graph`}>graph</Link></li>
+              <li><Link href={`/showcase/map`}>map</Link></li>
+            </ul>
+          </li>
+          <li className="mb-2">cheat sheet
+            <ul className="list-disc ml-4">
+              <li><Link href={`/cheat_sheet/flex`}>flex</Link></li>
+              <li><Link href={`/cheat_sheet/grid`}>grid</Link></li>
+            </ul>
+          </li>
         </ul>
+
+
+
       </div>
 
 
--- src/app/tool/font/page.tsx
+++ src/app/tool/font/page.tsx
@@ -51,33 +51,7 @@ export default function Page() {
 
       </tr>
     </table>
-    <div className="relative w-20 h-20">
-      <div className="w-full h-full rounded-full flex items-center justify-center relative overflow-hidden">
-        <span className="text-pink-500 text-lg font-bold z-10">75%</span>
-        <div
-          className="absolute top-0 left-0 w-full h-full border-4 rounded-full"
-          style={{
-            backgroundImage: 'conic-gradient(from 0deg at 50% 50%, #ff69b4 0%, #ff69b4 75%, #ccc 75%, #ccc 100%)',
-            borderImageSlice: 1,
-          }}
-        ></div>
-      </div>
-    </div>
-    <div className="relative w-20 h-20">
-      <div
-        style={{
-          backgroundImage: 'conic-gradient(from 0deg at 50% 50%, #ff69b4 0%, #ff69b4 75%, #ccc 75%, #ccc 100%)',
-          borderImageSlice: 1,
-        }}
-        className="w-full h-full rounded-full flex items-center justify-center relative overflow-hidden"
-      >
-        <div
-          className="absolute top-2 left-2 w-16 h-16 rounded-full"
-          style={{ backgroundColor: 'white' }}
-        ></div>
-        <span className="text-pink-500 text-lg font-bold z-10">75%</span>
-      </div>
-    </div>
+
 
   </div>
 
